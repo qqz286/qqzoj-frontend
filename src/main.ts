5ab2a9ec9eec75e665e42b4f30e8ac2ad35cba63
赵嘉琦 <13702479+zhao-jiaqi286@user.noreply.gitee.com>
2025-02-08 14:02:59 +0800

    init

--- src/main.ts
+++ src/main.ts
@@ -3,4 +3,7 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
-createApp(App).use(store).use(router).mount("#app");
+import ArcoVue from "@arco-design/web-vue";
+import "@arco-design/web-vue/dist/arco.css";
+
+createApp(App).use(store).use(ArcoVue).use(router).mount("#app");
